4c7bc9f4fa942c27734d14d90a1a3f71243b531f
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-06-30 00:20:30 +0530

    add question choprt

---
+++ Code Chef C++ Answers/CHOPRT.cpp
@@ -0,0 +1,26 @@
+//
+//  newfile.cpp
+//  Code Chef C++ Answers
+//
+//  Created by Pranjal Bhardwaj on 29/06/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int choprt(){
+    int iter,n1,n2;
+    cin >> iter;
+    for(int i = 0; i < iter ;i++){
+        cin >> n1 >> n2;
+        if(n1 > n2){
+            cout << ">" << endl;
+        } else if (n1 < n2){
+            cout << "<" << endl;
+        } else {
+            cout << "=" <<endl;
+        }
+    }
+    return 0;
+}
