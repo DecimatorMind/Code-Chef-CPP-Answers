41636458f323009dafb3b1988500304cf1af76dd
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-07-03 00:25:17 +0530

    add Question

---
+++ Code Chef C++ Answers/ZCOSCH.cpp
@@ -0,0 +1,23 @@
+//
+//  new.cpp
+//  Code Chef C++ Answers
+//
+//  Created by Pranjal Bhardwaj on 02/07/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int zcosch(){
+    int rank;
+    cin >> rank;
+    if (rank >= 1 and rank <= 50){
+        cout << 100 << endl;
+    } else if (rank >= 51 and rank <= 100){
+        cout << 50 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
